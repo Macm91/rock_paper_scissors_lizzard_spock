45e475715cfa14d872cbca843c7c3bded1752471
Megan Mandes <meganmandes@Megans-Air.lan>
2021-08-12 15:29:20 -0400

    added files for ai_player, battlefield, human_player, player and main. Created the player parent class.

---
+++ player.py
@@ -0,0 +1,10 @@
+class Player:
+    def __init__ (self):
+        self.name = ""
+        self.score = 0
+        self.gestures = ["rock", "paper","scissors", "lizard", "spock"]
+        self.gesture_choice = ""
+        
+
+    def gesture (self):
+     pass
